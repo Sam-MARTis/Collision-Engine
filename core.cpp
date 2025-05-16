41be6e18c1e2b8bf8fd7fd5350fee0608865637d
Sam-MARTis <samanthmartis@gmail.com>
2025-05-16 17:22:29 +0530

    Added main collision handling logic

--- core.cpp
+++ core.cpp
@@ -7,11 +7,14 @@
 
 #define DEFAULT_PARTICLE_COUNT 1000
 #define DIMENSIONS 2
+#define INTERNAL_DAMPING 1.0f
 #define RADIUS 5
+#define RADIUS_SQUARED_TIMES_FOUR 100
 #define STARTING_VELOCITY_RANGE 10
 #define BOUNDARY_X 800.0f
 #define BOUNDARY_Y 800.0f
 #define TIME_STEP 0.0005f
+
 
 #define DEBUG_MODE true
 
@@ -72,6 +75,34 @@ void updateGrid(float *pos, unsigned int *grid, int p_count, const int countx, c
     }
 }
 
+void handle_collision(float *pos, float *vel, int id1, int id2){
+    const int p1 = (id1-1)*2;
+    const int p2 = (id2-1)*2;
+    const float dx = pos[p2] - pos[p1];
+    const float dy = pos[p2+1] - pos[p1+1];
+    const float distance_squared = pow(dx, 2) + pow(dy, 2);
+    if(distance_squared> RADIUS_SQUARED_TIMES_FOUR){
+        return;
+    }
+    const float distance = sqrtf(distance_squared);
+    if (distance < 1e-6f) return;
+    const float nx = dx/distance;
+    const float ny = dy/distance;
+    const float dot_product = nx* (vel[p1] - vel[p2]) + ny*(vel[p1+1] - vel[p2+1]);
+    vel[p1] -= 1 * dot_product * nx *INTERNAL_DAMPING;
+    vel[p1+1] -= 1 * dot_product * ny *INTERNAL_DAMPING;
+    vel[p2] += 1 * dot_product * nx*INTERNAL_DAMPING;
+    vel[p2+1] += 1 * dot_product * ny*INTERNAL_DAMPING;
+    pos[p1] -= (nx * ( RADIUS - distance));
+    pos[p1+1] -= (ny * ( RADIUS - distance));
+    pos[p2] += (nx * ( RADIUS - distance));
+    pos[p2+1] += (ny * ( RADIUS - distance));
+}
+
+void n_square_collision_solve(){
+
+}
+
 
 
 int main(int argc, char **argv)
@@ -104,7 +135,7 @@ int main(int argc, char **argv)
     Why? Because theres a chance of strong overlap in the beggining. 
     Once sorted manually, the grid can take over and, assuming dt is small, there should be no collisions. 
     */
-
+    
     
 
     return 0;
