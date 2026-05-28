c7018a9c8edbe1f54e41b4af369c026cea47b4f2
Sam-MARTis <samanthmartis@gmail.com>
2026-05-28 18:25:01 +0530

    Core funcationality complete

--- V2/core-functions.cpp
+++ V2/core-functions.cpp
@@ -145,94 +145,116 @@ void ParticleSystem::handleCollisionsFromUpdatedGrid(const int &num_global_itera
 void ParticleSystem::handleCollisionsFromUpdatedGridParallel(const int &num_global_iterations, const int &num_cell_iterations)
 {
     bool something_done_in_this_global_iteration = true;
+    const int num_threads = omp_get_max_threads();
+    const int num_cols_each_thread = ceil(0.5f * grid_cols / num_threads);
     for (int global_iter_count = 0; (global_iter_count < num_global_iterations) && something_done_in_this_global_iteration; global_iter_count++)
     {
+        something_done_in_this_global_iteration = false;
+        bool something_done_in_this_global_iteration_array[num_threads] = {false};
         bool something_done_in_this_cell_iteration = false;
-        const int num_threads = omp_get_max_threads();
-        const int num_cols_each_thread = ceil(0.5f * grid_cols / num_threads);
         for (int type_iter = 0; type_iter < 2; type_iter++)
         {
 // const int thread_id = ;
-#pragma omp parallel 
-{
-            const int starting_point = 1 + ((2 * omp_get_thread_num() + type_iter) * num_cols_each_thread);
-            if (starting_point <= grid_cols - 2){
-            const int ending_point = minOfTwoInts(starting_point + num_cols_each_thread, grid_cols - 1);
-            for (int i = starting_point; i < ending_point; i++)
+#pragma omp parallel
             {
-                for (int j = 1; j < grid_rows - 1; j++)
+                std::vector<unsigned int> all_particles;
+                all_particles.reserve(RESERVE_SURROUNDING_PARTCILES + RESERVE_CURRENT_CELL_PARTICLES);
+                const int starting_point = 1 + ((2 * omp_get_thread_num() + type_iter) * num_cols_each_thread);
+                if (starting_point <= grid_cols - 2)
                 {
-                    const int grid_cell_index = flattenCoords(i, j);
-                    const std::vector<unsigned int> &particles_in_this_cell = collision_grid[grid_cell_index];
-                    if (particles_in_this_cell.size() == 0)
-                        continue;
-                    int num_surrounding_particles = 0;
-                    for (int di = -1; di <= 1; di++)
+                    const int ending_point = minOfTwoInts(starting_point + num_cols_each_thread, grid_cols - 1);
+                    for (int i = starting_point; i < ending_point; i++)
                     {
-                        for (int dj = -1; dj <= 1; dj++)
+                        for (int j = 1; j < grid_rows - 1; j++)
                         {
-                            if ((dj != 0) || (di != 0))
+                            all_particles.clear();
+                            const int grid_cell_index = flattenCoords(i, j);
+                            const std::vector<unsigned int> &particles_in_this_cell = collision_grid[grid_cell_index];
+                            if (particles_in_this_cell.size() == 0)
+                                continue;
+                            int num_surrounding_particles = 0;
+                            for (int di = -1; di <= 1; di++)
                             {
-                                const int idx = flattenCoords(i + di, j + dj);
-                                num_surrounding_particles += collision_grid[idx].size();
-                            }
-                        }
-                    }
-                    std::vector<unsigned int> surrounding_particles;
-                    surrounding_particles.reserve(num_surrounding_particles);
-                    for (int di = -1; di <= 1; di++)
-                    {
-                        for (int dj = -1; dj <= 1; dj++)
-                        {
-                            if ((dj != 0) || (di != 0))
-                            {
-                                const std::vector<unsigned int> &particles_here = collision_grid[flattenCoords(i + di, j + dj)];
-                                surrounding_particles.insert(surrounding_particles.end(), particles_here.begin(), particles_here.end());
-                            }
-                        }
-                    }
-                    std::vector<unsigned int> all_particles;
-                    all_particles.reserve(num_surrounding_particles + particles_in_this_cell.size());
-                    all_particles.insert(all_particles.end(), surrounding_particles.begin(), surrounding_particles.end());
-                    all_particles.insert(all_particles.end(), particles_in_this_cell.begin(), particles_in_this_cell.end());
-                    const float diam_sq = PARTICLE_RADIUS * PARTICLE_RADIUS * 4.0f;
-                    bool something_done_in_this_cell_iteration = true;
-                    for (int cell_iter_count = 0; (cell_iter_count < num_cell_iterations) && something_done_in_this_cell_iteration; cell_iter_count++)
-                    {
-                        something_done_in_this_cell_iteration = false;
-                        for (const unsigned int &id_self : particles_in_this_cell)
-                        {
-                            ParticleKinematics &this_particle = particle_dynamics[id_self];
-                            for (unsigned int &id_other : all_particles)
-                            {
-                                if (id_self != id_other)
+                                for (int dj = -1; dj <= 1; dj++)
                                 {
-                                    ParticleKinematics &other_particle = particle_dynamics[id_other];
-                                    const sf::Vector2f dr = other_particle.pos - this_particle.pos;
-                                    const float ds_sq = sfVectorNormSq(dr);
-                                    if (ds_sq < diam_sq)
+                                    // if ((dj != 0) || (di != 0))
+                                    // {
+                                    //     const int idx = flattenCoords(i + di, j + dj);
+                                    //     num_surrounding_particles += collision_grid[idx].size();
+                                    // }
+
+                                    for (const int &id : collision_grid[flattenCoords(i + di, j + dj)])
                                     {
-                                        something_done_in_this_cell_iteration = true;
-                                        const float ds = sqrtf(ds_sq);
-                                        const float inv_ds = 1.0f / (ds);
-                                        const float overlap_each_particle = PARTICLE_RADIUS - ds * 0.5f;
-                                        const sf::Vector2f delta = dr * (overlap_each_particle * inv_ds);
-                                        this_particle.pos -= delta;
-                                        other_particle.pos += delta;
+                                        all_particles.push_back(id);
                                     }
                                 }
                             }
+                            if (all_particles.size() <= 1)
+                                continue;
+
+                            // std::vector<unsigned int> surrounding_particles;
+                            // surrounding_particles.reserve(num_surrounding_particles);
+                            // for (int di = -1; di <= 1; di++)
+                            // {
+                            //     for (int dj = -1; dj <= 1; dj++)
+                            //     {
+                            //         if ((dj != 0) || (di != 0))
+                            //         {
+                            //             const std::vector<unsigned int> &particles_here = collision_grid[flattenCoords(i + di, j + dj)];
+                            //             surrounding_particles.insert(surrounding_particles.end(), particles_here.begin(), particles_here.end());
+                            //         }
+                            //     }
+                            // }
+                            // std::vector<unsigned int> all_particles;
+                            // all_particles.reserve(num_surrounding_particles + particles_in_this_cell.size());
+                            // all_particles.insert(all_particles.end(), surrounding_particles.begin(), surrounding_particles.end());
+                            // all_particles.insert(all_particles.end(), particles_in_this_cell.begin(), particles_in_this_cell.end());
+                            const float diam_sq = PARTICLE_RADIUS * PARTICLE_RADIUS * 4.0f;
+                            bool something_done_in_this_cell_iteration = true;
+                            for (int cell_iter_count = 0; (cell_iter_count < num_cell_iterations) && something_done_in_this_cell_iteration; cell_iter_count++)
+                            {
+                                something_done_in_this_cell_iteration = false;
+                                for (const unsigned int &id_self : particles_in_this_cell)
+                                {
+                                    ParticleKinematics &this_particle = particle_dynamics[id_self];
+                                    for (unsigned int &id_other : all_particles)
+                                    {
+                                        if (id_self != id_other)
+                                        {
+                                            ParticleKinematics &other_particle = particle_dynamics[id_other];
+                                            const sf::Vector2f dr = other_particle.pos - this_particle.pos;
+                                            const float ds_sq = sfVectorNormSq(dr);
+                                            if (ds_sq < diam_sq)
+                                            {
+                                                something_done_in_this_cell_iteration = true;
+                                                const float ds = sqrtf(ds_sq);
+                                                const float inv_ds = 1.0f / (ds);
+                                                const float overlap_each_particle = PARTICLE_RADIUS - ds * 0.5f;
+                                                const sf::Vector2f delta = dr * (overlap_each_particle * inv_ds);
+                                                this_particle.pos -= delta;
+                                                other_particle.pos += delta;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if (something_done_in_this_cell_iteration)
+                            {
+                                something_done_in_this_global_iteration_array[omp_get_thread_num()] = true;
+                            }
                         }
                     }
-                    if (something_done_in_this_cell_iteration)
-                        something_done_in_this_global_iteration = true;
                 }
             }
-        }}
+        }
+        for(int l=0; l<num_threads; l++){
+            if(something_done_in_this_global_iteration_array[l]){
+                something_done_in_this_global_iteration = true;
+                break;
+            }
+        }
     }
 }
-}
-
 
 void ParticleSystem::solveCollisions(const int collision_num_global_iterations, const int collision_num_cell_iterations)
 {
